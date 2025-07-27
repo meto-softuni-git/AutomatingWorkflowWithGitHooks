3709cda7a9e91c0690710300125f5152990a7905
Metodil <Metodil@hotmail.com>
2025-07-27 10:15:48 +0300

    Add push pre-commit and tests

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class MathSum
+{
+    public static int Add(int a, int b) => a + b;
+}
---
+++ Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace AutomatingWorkflowWithGitHooks
+{
+    class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckMathSum()
+        {
+            var result = MathSum.Add(2, 3);
+            Assert.Equals(5, result);
+        }
+    }
+}
